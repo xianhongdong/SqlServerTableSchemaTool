fa9605863d658b5aee37cde596a597f3d463db3d
xianhongdong <xianhongdong@gmail.com>
2013-10-26 11:45:57 +0800

    add code

---
+++ DatabaseSchemaTool/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace DatabaseSchemaTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.textBoxDatabase = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnSelect = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.ColumnTableName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonLoad);
+            this.groupBox1.Controls.Add(this.textBoxDatabase);
+            this.groupBox1.Controls.Add(this.textBoxPassword);
+            this.groupBox1.Controls.Add(this.textBoxUserName);
+            this.groupBox1.Controls.Add(this.textBoxServer);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(520, 90);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection Config";
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(428, 51);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 4;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxDatabase
+            // 
+            this.textBoxDatabase.Location = new System.Drawing.Point(283, 13);
+            this.textBoxDatabase.Name = "textBoxDatabase";
+            this.textBoxDatabase.Size = new System.Drawing.Size(120, 21);
+            this.textBoxDatabase.TabIndex = 1;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(283, 53);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(120, 21);
+            this.textBoxPassword.TabIndex = 3;
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(71, 59);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(120, 21);
+            this.textBoxUserName.TabIndex = 2;
+            this.textBoxUserName.Text = "sa";
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(71, 13);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(120, 21);
+            this.textBoxServer.TabIndex = 0;
+            this.textBoxServer.Text = "(local)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "UserName:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "DataBase:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnSelect,
+            this.ColumnTableName});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 125);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(516, 225);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ColumnSelect
+            // 
+            this.ColumnSelect.DataPropertyName = "select";
+            this.ColumnSelect.HeaderText = "选择";
+            this.ColumnSelect.Name = "ColumnSelect";
+            // 
+            // ColumnTableName
+            // 
+            this.ColumnTableName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColumnTableName.DataPropertyName = "table_name";
+            this.ColumnTableName.HeaderText = "表名称";
+            this.ColumnTableName.Name = "ColumnTableName";
+            this.ColumnTableName.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColumnTableName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(355, 378);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(75, 23);
+            this.buttonExport.TabIndex = 1;
+            this.buttonExport.Text = "Export Schema";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClose.Location = new System.Drawing.Point(460, 378);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonClose;
+            this.ClientSize = new System.Drawing.Size(540, 413);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Database Schema Tool";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.TextBox textBoxDatabase;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColumnSelect;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTableName;
+    }
+}
+
---
+++ DatabaseSchemaTool/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+namespace DatabaseSchemaTool
+{
+    public partial class Form1 : Form
+    {
+        private DataSet tableNameDataSet = new System.Data.DataSet();
+        DataSet exportSchemaTableDataSet = new System.Data.DataSet();
+        SqlDataAdapter da = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strServer = textBoxServer.Text;
+            string strDb = textBoxDatabase.Text;
+            string strUserName = textBoxUserName.Text;
+            string strPassword = textBoxPassword.Text;
+            string connStr = string.Format("Data Source={0};Initial Catalog={1};User ID={2};Password={3}"
+                ,strServer,strDb,strUserName,strPassword);
+            SqlConnection conn = new SqlConnection(connStr);
+            //dataGridView1.DataSource = null;
+            string sql = "select 0 as 'select', table_name from information_schema.tables";
+            try
+            {
+                tableNameDataSet.Tables.Clear();
+                da = new SqlDataAdapter(sql, conn);
+                da.Fill(tableNameDataSet);
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            string tableName = tableNameDataSet.Tables[0].TableName;
+            
+            dataGridView1.DataSource = tableNameDataSet.Tables[0];
+            //dataGridView1.DataMember = tableName;
+            //dataGridView1.Refresh();
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            DataTable dt = tableNameDataSet.Tables[0];
+            foreach(DataRow dr in dt.Rows)
+            {
+                bool isSelect = (int)dr["select"] == 0 ? false : true;
+                string strName = dr["table_name"] as string;
+                if (!isSelect)
+                    continue;
+                string sql = string.Format("select * from {0} where 1=2", strName);
+                da.SelectCommand.CommandText = sql;
+                da.Fill(exportSchemaTableDataSet,strName);
+            }
+            if(exportSchemaTableDataSet.Tables.Count < 1)
+            {
+                MessageBox.Show("请选择导出的表!");
+                return;
+            }
+            SaveFileDialog sfDlg = new SaveFileDialog();
+            sfDlg.DefaultExt = "xml";
+            sfDlg.Filter = "xml file (*.xml)|*.xml|All files (*.*)|*.*";
+            if(sfDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                exportSchemaTableDataSet.WriteXmlSchema(sfDlg.FileName);
+                MessageBox.Show("导出成功!");
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
